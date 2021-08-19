11039e689cd494814a57a0926041e64bcef36ca2
Alim Yuzbashev <a.yuzbashev@pnn.in.ua>
2021-08-19 18:01:33 +0300

    EvolvStoreNewTests: testAllocationsAreLoadedCorrectly assert that publisher finished

--- EvolvSwiftSDK/EvolvSwiftSDKTests/Common-Tests/EvolvStoreNewTests.swift
+++ EvolvSwiftSDK/EvolvSwiftSDKTests/Common-Tests/EvolvStoreNewTests.swift
@@ -1,8 +1,19 @@
 //
-//  EvolvStoreNewTests.swift
-//  EvolvSwiftSDKTests
+//  EvolvStoreTest.swift
 //
-//  Created by Alim Yuzbashev on 19.08.2021.
+//  Copyright (c) 2021 Evolv Technology Solutions
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 import XCTest
@@ -77,6 +88,7 @@ class EvolvStoreNewTests: XCTestCase {
         EvolvStoreImpl.initialize(evolvContext: context, evolvAPI: evolvAPIMock)
             .sink(receiveCompletion: { publisherCompletion in
                 XCTAssertNotNil(evolvStore)
+                XCTAssertTrue(publisherCompletion.isFinished)
                 
                 if case .finished = publisherCompletion {
                     XCTAssertEqual(self.evolvConfiguration, evolvStore.evolvConfiguration)
