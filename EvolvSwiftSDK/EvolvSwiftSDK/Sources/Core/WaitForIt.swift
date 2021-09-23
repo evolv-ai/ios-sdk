6f4ca2df6fb8ac84ccf80b89d7d7b51a5febfacd
Alim Yuzbashev <45521753+alimyuz@users.noreply.github.com>
2021-09-23 18:46:18 +0300

    WaitForIt class

--- EvolvSwiftSDK/EvolvSwiftSDK/Sources/Core/WaitForIt.swift
+++ EvolvSwiftSDK/EvolvSwiftSDK/Sources/Core/WaitForIt.swift
@@ -16,13 +16,70 @@
 //  limitations under the License.
 //
 
-
 import Foundation
 
 class WaitForIt {
+    typealias HandlerFunc = ((Any) -> Void)
     
-    var scopeHandlers = OrderedDictionary<String, [String: Any]>()
-    var scopedOnceHandlers = OrderedDictionary<String, [String: Any]>()
-    var scopedPayloads = OrderedDictionary<String, [String: Any]>()
+    private var scopedHandlers = OrderedDictionary<AnyHashable, [AnyHashable: [HandlerFunc]]>()
+    private var scopedOnceHandlers = OrderedDictionary<AnyHashable, [AnyHashable: [HandlerFunc]]>()
+    private var scopedPayloads = OrderedDictionary<AnyHashable, [AnyHashable: Any]>()
     
+    static let shared = WaitForIt()
+    
+    func waitFor(scope: AnyHashable, it: AnyHashable, handler: @escaping HandlerFunc) {
+        ensureScope(scope)
+        
+        if scopedHandlers[scope]?[it] == nil {
+            scopedHandlers[scope]?[it] = [handler]
+        } else {
+            scopedHandlers[scope]?[it]?.append(handler)
+        }
+        
+        if let payload = scopedPayloads[scope]?[it] {
+            handler(payload)
+        }
+    }
+    
+    func waitOnceFor(scope: AnyHashable, it: AnyHashable, handler: @escaping HandlerFunc) {
+        ensureScope(scope)
+        
+        if let payload = scopedPayloads[scope]?[it] {
+            handler(payload)
+            return
+        }
+        
+        if scopedOnceHandlers[scope]?[it] == nil {
+            scopedOnceHandlers[scope]?[it] = [handler]
+        } else {
+            scopedOnceHandlers[scope]?[it]?.append(handler)
+        }
+    }
+    
+    func emit(scope: AnyHashable, it: AnyHashable, _ other: Any...) {
+        ensureScope(scope)
+        
+        var payload = other
+        payload.insert(it, at: 0)
+        scopedPayloads[scope]?[it] = payload
+        
+        scopedOnceHandlers[scope]?[it]?.forEach { $0(payload) }
+        scopedOnceHandlers[scope] = nil
+        
+        scopedHandlers[scope]?[it]?.forEach { $0(payload) }
+    }
+    
+    func destroyScope(scope: AnyHashable) {
+        scopedHandlers[scope] = nil
+        scopedOnceHandlers[scope] = nil
+        scopedPayloads[scope] = nil
+    }
+    
+    private func ensureScope(_ scope: AnyHashable) {
+        guard scopedHandlers[scope] == nil else { return }
+        
+        scopedHandlers[scope] = [:]
+        scopedOnceHandlers[scope] = [:]
+        scopedPayloads[scope] = [:]
+    }
 }
