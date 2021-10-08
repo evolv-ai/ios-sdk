33b3a0c04815dbde6b0902846b00ca4ad12d63ab
Alim Yuzbashev <45521753+alimyuz@users.noreply.github.com>
2021-10-08 15:36:01 +0300

    remove comments

--- EvolvSwiftSDK/EvolvSwiftSDK/Sources/Protocols/EvolvContextContainer.swift
+++ EvolvSwiftSDK/EvolvSwiftSDK/Sources/Protocols/EvolvContextContainer.swift
@@ -40,26 +40,6 @@ protocol EvolvContextContainer {
     
     var events: [EvolvCustomEvent] { get set }
     
-//    /// Checks if the specified key is currently defined in the context.
-//    /// - Parameter key: The key to check.
-//    func contains(key: Bool)
-//
-//    /// Checks if the specified key is currently defined in the context.
-//    /// - Parameter key: The key associated with the value to retrieve.
-//    func get(key: String)
-//
-//    /// Adds value to specified array in context. If array doesn't exist its created and added to.
-//    /// - Parameter key: The array to add to.
-//    /// - Parameter value: Value to add to the array.
-//    /// - Parameter local: If true, the value will only be added to the localContext.
-//    /// - Parameter limit: Max length of array to maintain.
-//    func pushToArray(key: String, value: String, local: Bool, limit: Int)
-//
-//    /// Remove a specified key from the context.
-//    /// Note: This will cause the effective genome to be recomputed.
-//    /// - Parameter key: The key to remove from the context.
-//    func remove(key: String)
-    
     /// Computes the effective context from the local and remote contexts.
     func resolve() -> [String: Any]
     
